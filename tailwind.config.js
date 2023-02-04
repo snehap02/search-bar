ab68496b9e2259996f04877090b8261870359d22
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-02-04 19:30:32 +0530

    first section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
       colors:{
         blue: '#6050DC',
         darkPurple: '#2a0378',
-        urbanBlue: '#AFDBF5'
+        urbanBlue: '#AFDBF5',
+        light: '#bbd2ff'
       },
       backgroundImage:{
         cardBg: 'linear-gradient(90deg, rgba(58,0,159,1) 17%, rgba(54,20,208,0.9809173669467787) 100%)'
