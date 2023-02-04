dc34fd4842268cf5a08fd5388934e18b59733f28
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-02-04 19:02:10 +0530

    fonts and symbols added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        blue: '#6050DC',
+        darkPurple: '#2a0378',
+        urbanBlue: '#AFDBF5'
+      },
+      backgroundImage:{
+        cardBg: 'linear-gradient(90deg, rgba(58,0,159,1) 17%, rgba(54,20,208,0.9809173669467787) 100%)'
+      },
+      screens:{
+        'sm-0': '450px'
+      },
+      fontFamily:{
+        montserrat: 'Montserrat',
+        poppins: 'Poppins',
+        raleway: 'Raleway'
+      }
+    },
   },
   plugins: [],
 }
